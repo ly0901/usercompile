d494adddc5ebe5b8eef4aaea3a03a53727375b31
luoxieting <l_xieting@126.com>
2016-06-08 17:23:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/usercompile/TestView.java
@@ -0,0 +1,23 @@
+package com.example.administrator.usercompile;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by lxt on 2016-6-8.
+ */
+public class TestView extends TextView {
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
